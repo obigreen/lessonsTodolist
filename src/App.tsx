7943bd88c47b4250d722b1a758493c82049fb653
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-02 21:38:09 +0300

    remove for todo 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css'
 import {Todolist} from './Todolist'
 
-// CRUT операции
-// Create
-// Read
-// Update
-// Delete
 
 export type TaskType = {
     id: number
@@ -15,126 +10,19 @@ export type TaskType = {
 }
 
 
-export type FilterValuesType = "All" | "Active" | "Completed"
-
-
 function App() {
 
-    const todoListTitle = "What to learn"
-    // global state
-    let [tasks, setTasks] = useState([
-        {id: 1, title: "CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "React", isDone: false},
-        {id: 4, title: "Redux", isDone: false}
-    ]);
+    const tasks1 = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'ReactJS', isDone: false }
+    ]
 
-    // удаление tasks
-    function removeTasak(taskId: number) {
-        // через filter
-        let resutlTasks = tasks.filter(t => t.id !== taskId) // к примеру если
-        // id не равно(!==) выбранному для удаления id то вернет true
-        // если равен то false
-        setTasks(resutlTasks)
-
-        // через цикл
-        // const newState = []
-        // for (let i = 0; i < tasks.length; i++) {
-        //     if (tasks[i].id !== taskId) {
-        //         newState.push(tasks[i])
-        //     }
-        // }
-        // setTasks(newState)
-    }
-
-    // фильтрация списков по кнопкам
-    // local state
-    const [filter, setFilter] = useState<FilterValuesType>("All")
-
-    const changeFilter = (nextFilter: FilterValuesType) => {
-        setFilter(nextFilter)
-    }
-    // UI
-    const getTasksForTodolist = (allTasks: Array<TaskType>, nextFilterValue: FilterValuesType) => {
-        switch (nextFilterValue) {
-            case "Active":
-                //!t.isDone упрощена от t.isDone === false, если сокращать, обязательно отследить что бы у false стояло !-не, а то у меня 2 кнопки отрабатывали Completed так как я просто сократил t.isDone оба =)
-                return allTasks.filter(t => !t.isDone);
-            case "Completed":
-                return allTasks.filter(t => t.isDone);
-            default:
-                return allTasks;
-        }
-    }
-    const tasksForTodolist = getTasksForTodolist(tasks, filter);
-
-
-    // function changeFilter(value: FilterValuesType) {
-    //     setFilter(value);
-    // }
-    // let tasksForTodolist = tasks;
-    // if (filter === "Completed") {
-    //     tasksForTodolist = tasks.filter(t => t.isDone);
-    // }
-    //
-    // if (filter === "Active") {
-    //     tasksForTodolist = tasks.filter(t => t.isDone);
-    // }
-
-
-    // ==========================================================================
     return (
         <div className={'App'}>
-            <Todolist title={todoListTitle}
-                      tasks={tasksForTodolist}
-                // пробрасываем removeTasak в котором resutlTasks
-                // результат фильтрации tasks
-                // removeTasak это props в который мы передаем f removeTasak
-                      removeTasak={removeTasak}
-                      changeFilter={changeFilter}
-            />
+           <Todolist title={"What to learn"} tasks={tasks1}/>
         </div>
     )
 }
 
 export default App;
-
-
-// Конспект по "1 todolist for studients 01"
-
-// Суть React в создании своих выдуманных тегов, например App который отрисовывается в
-// index.tsx. Компоненты это function которая отрисовывает jsx разметку
-// comm + shift + вернуть откат comm + z
-// в {} только js файлы, остальное не обязательно пример <input type='checkbox' checked={true}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,101 +1,40 @@
-import React, {JSX} from 'react'
-import {FilterValuesType, TaskType} from "./App";
+import React from 'react'
+import {TaskType} from "./App";
+import {Button} from "./Button";
 
 
 
 type PropsType = {
     title: string
     tasks: TaskType[]
-    //     ||
-    //     tasks: Array<TaskType>
-    //     void - если функция ничего не возвращает, не имеет return
-    // вместо taskId можно любое, главное для TS тип, здесь number
-    removeTasak: (taskId: number) => void
-    changeFilter: (nextFilter: FilterValuesType) => void
-    // changeFilter: (value: FilterValuesType) => void
 }
 
 
-export const Todolist = (props: PropsType) => {
-
-
-    // map через цикл-------------------------------------------------------------------
-    // const taskslist: Array<JSX.Element> = []
-    // for (let i = 0; i < props.tasks.length; i++) {
-    //     taskslist.push(
-    //         <li><input type='checkbox' checked={props.tasks[i].isDone}/>
-    //             <span>{props.tasks[i].title}</span>
-    //             <button onClick={() => {
-    //                 props.removeTasak(props.tasks[i].id)
-    //             }}>x
-    //             </button>
-    //         </li>
-    //     )
-    // }
-
-
-    // или такой map, вообще метод .map() является более предпочтительным
-    const taskslist: Array<JSX.Element> = props.tasks.map(t => {
-
-        const removeTaskHandler = () => props.removeTasak(t.id)
-        return (
-            <li>
-                <input type='checkbox' checked={t.isDone}/>
-                <span>{t.title}</span>
-                {/*<button onClick={() => {props.removeTasak(t.id)}}>x</button>*/}
-                <button onClick={removeTaskHandler}>x</button>
-            </li>
-        )
-    })
-
-    // map через цикл-------------------------------------------------------------------
+export const Todolist = ({title, tasks}: PropsType) => {
 
     return (
         <div className='todolist'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                {/* map через цикл-------------------------------------------------------------------*/}
-                {taskslist}
-                {/* map через цикл-------------------------------------------------------------------*/}
-
-
-                {/*{*/}
-                {/*    props.tasks.map(t => <li><input type='checkbox' checked={t.isDone}/>*/}
-                {/*            <span>{t.title}</span>*/}
-                {/*            <button onClick={() => {props.removeTasak(t.id)}}>x</button>*/}
-                {/*        </li>*/}
-                {/*    )*/}
-                {/*}*/}
+                {
+                    tasks.map(t => <li><input type='checkbox' checked={t.isDone}/>
+                            <span>{t.title}</span>
+                            <button>x</button>
+                        </li>
+                    )
+                }
             </ul>
 
             <div>
-                <button onClick={() => props.changeFilter("All")}>All</button>
-                <button onClick={() => props.changeFilter("Active")}>Active</button>
-                <button onClick={() => props.changeFilter("Completed")}>Completed</button>
+                <Button title={'All'} />
+                <Button title={'Active'} />
+                <Button title={'Completed'} />
             </div>
         </div>
     )
 }
 
-
-// 1 твердо установленное значение index(а) []
-//
-//                 <li>
-//                     <input type='checkbox' checked={tasks[1].isDone}/>
-//                     <span>{tasks[1].title}</span>
-//                 </li>
-
-{/*<li>*/
-}
-{/*    /!*берем из [] 0вой (1ый) объект []ва конкретно isDone*!/*/
-}
-{/*    <input type='checkbox' checked={tasks[0].isDone}/>*/
-}
-{/*    <span>{tasks[0].title}</span>*/
-}
-{/*</li>*/
-}
