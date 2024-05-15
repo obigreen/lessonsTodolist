334efcdbedc7e9bc608f07a6b31ab4101ad7f2e7
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-15 17:47:53 +0300

    added disadled on Button.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,11 +2,12 @@ type ButtonPropsType = {
     title: string
     // void function ничего не возвращает
     onClick: () => void
+    disabled?: boolean
 }
 
-export const Button = ({ title, onClick }: ButtonPropsType) => {
+export const Button = ({ title, onClick, disabled }: ButtonPropsType) => {
 
     const onClickHandler = () => {onClick()}
 
-    return <button onClick={onClickHandler}>{title}</button>
+    return <button onClick={onClickHandler} disabled={disabled}>{title}</button>
 }
