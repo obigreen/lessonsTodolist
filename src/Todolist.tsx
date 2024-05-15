d0ea0e17f7b733d3d6640575857dc8c501bb0928
Sergey Budyakin <youshtuchka@gmail.com>
2024-05-15 17:48:34 +0300

    create controls input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,6 @@
 import {FilterValuesType, TaskType} from "./App";
 import {Button} from "./Button";
+import {useState, KeyboardEvent, ChangeEvent} from "react";
 
 
 type PropsType = {
@@ -13,17 +14,67 @@ type PropsType = {
 
 export const Todolist = ({title, tasks, removeTask, changeFilter, addTask}: PropsType) => {
 
+    //ui logic
+    const [taskTitle, setTaskTitle] = useState("")
+
+
+    const addTaskHandler = () => {
+        addTask(taskTitle)
+        setTaskTitle("")
+    }
+
+    // здесь мы убирали обработчик
+    // const addTaskOnkeyUpHandler = taskTitle.length === 0
+    //     ? undefined : (e: KeyboardEvent<HTMLInputElement>) => {
+    //         if (e.key === "Enter") {
+    //             addTaskHandler()
+    //         }
+    //     }
+
+    //еще варик
+    // обработчик срабатывает, но ничего не добавляет
+    const addTaskOnKeyUpHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && taskTitle) {
+            addTaskHandler()
+        }
+    }
+
+
+    // этот вынес
+    const changeTaskTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTaskTitle(e.currentTarget.value)
+    const isAddBtnDis = taskTitle.length === 0 || taskTitle.length > 15
 
     return (
         <div className='todolist'>
             <h2>{title}</h2>
             <div>
-                <input/>
-                <Button title={"+"} onClick={() => {addTask("New task")}}/>
-            </div>
-            <ul>
+                <input value={taskTitle}
+                       onChange={changeTaskTitleHandler}
+
+                    // onKeyUp={e => {
+                    //     if (e.key === "Enter") {
+                    //         addTaskHandler()
+                    //     }
+                    // }}/>
+                       onKeyUp={addTaskOnKeyUpHandler}/>
+
+                <Button title={"+"}
+                        onClick={addTaskHandler}
+                        disabled={isAddBtnDis}/>
+
+                {/*{taskTitle.length > 10 && <div>recomended 10 charteres</div>}*/}
+
+
                 {
-                    tasks.map(t => {
+                    taskTitle.length > 15
+                        ? <div>max 15 characters</div>
+                        : taskTitle.length > 10 && <div>recommended 10 characters</div>
+                }
+            </div>
+
+            {tasks.length === 0 ? <p>not tasks</p> :
+                <ul>
+                    {tasks.map(t => {
 
                             const onClickRemoveTaskHandler = () => {
                                 removeTask(t.id);
@@ -37,9 +88,9 @@ export const Todolist = ({title, tasks, removeTask, changeFilter, addTask}: Prop
                                 </li>
                             )
                         }
-                    )
-                }
-            </ul>
+                    )}
+                </ul>
+            }
 
             <div>
                 <Button title={"All"} onClick={() => {
